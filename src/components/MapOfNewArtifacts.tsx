9d40589f1a4119f9e0f5b26475bc56032881455e
simmetr <vm.certo@gmail.com>
2025-12-26 10:58:59 +0700

    feat: добавлены файлы изображений

--- src/components/MapOfNewArtifacts.tsx
+++ src/components/MapOfNewArtifacts.tsx
@@ -6,13 +6,13 @@ export const MapOfNewArtifacts = () => {
         <div className="map-of-new-artifacts">
             <h2 className='map-of-new-artifacts__heading'>Новые артефакты</h2>
             <div className='map-of-new-artifacts__map-with-artifacts'>
-                <img src='' alt=''/>
-                <img src='' alt=''/>
-                <img src='' alt=''/>
-                <img src='' alt=''/>
-                <img src='' alt=''/>
-                <img src='' alt=''/>
-                <img src='' alt=''/>
+                <img src='./for-artifact-card/example-5.webp' alt='example-5'/>
+                <img src='./for-artifact-card/example-6.webp' alt='example-6'/>
+                <img src='./for-artifact-card/example-7.webp' alt='example-7'/>
+                <img src='./for-artifact-card/example-8.webp' alt='example-8'/>
+                <img src='./for-artifact-card/example-9.webp' alt='example-9'/>
+                <img src='./for-artifact-card/example-10.webp' alt='example-10'/>
+                <img src='./for-artifact-card/example-11.webp' alt='example-11'/>
                 <p className=''>5.3</p>
                 <p className=''>1.6</p>
                 <p className=''>6.3</p>
