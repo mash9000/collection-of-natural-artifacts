4cbc519f2bd8cbee496174b1309e95085b7a997a
simmetr <vm.certo@gmail.com>
2025-12-26 12:39:09 +0700

    feat, style: размещение четвёртого изображения с рейтом

--- src/components/map-of-new-artifacts/MapOfNewArtifacts.tsx
+++ src/components/map-of-new-artifacts/MapOfNewArtifacts.tsx
@@ -6,10 +6,12 @@ import '../../styles/map-of-new-artifacts/__description-block/__heading/map-of-n
 import '../../styles/map-of-new-artifacts/__map-with-artifacts/map-of-new-artifacts__map-with-artifacts.scss';
 import '../../styles/map-of-new-artifacts/__map-with-artifacts/__img/map-of-new-artifacts__map-with-artifacts__img--example-5.scss';
 import '../../styles/map-of-new-artifacts/__map-with-artifacts/__img/map-of-new-artifacts__map-with-artifacts__img--example-9.scss';
+import '../../styles/map-of-new-artifacts/__map-with-artifacts/__img/map-of-new-artifacts__map-with-artifacts__img--example-10.scss';
 import '../../styles/map-of-new-artifacts/__map-with-artifacts/__img/map-of-new-artifacts__map-with-artifacts__img--example-11.scss';
 import '../../styles/map-of-new-artifacts/__map-with-artifacts/__rate/map-of-new-artifacts__map-with-artifacts__rate--rate-1.scss';
 import '../../styles/map-of-new-artifacts/__map-with-artifacts/__rate/map-of-new-artifacts__map-with-artifacts__rate--rate-2.scss';
 import '../../styles/map-of-new-artifacts/__map-with-artifacts/__rate/map-of-new-artifacts__map-with-artifacts__rate--rate-3.scss';
+import '../../styles/map-of-new-artifacts/__map-with-artifacts/__rate/map-of-new-artifacts__map-with-artifacts__rate--rate-4.scss';
 
 import {ReadMore} from "./ReadMore.tsx";
 
@@ -20,10 +22,12 @@ export const MapOfNewArtifacts = () => {
             <div className='map-of-new-artifacts__map-with-artifacts'>
                 <img className='map-of-new-artifacts__map-with-artifacts__img--example-5' src='./for-artifact-card/example-5.webp' alt='example-5'/>
                 <img className='map-of-new-artifacts__map-with-artifacts__img--example-9' src='./for-artifact-card/example-9.webp' alt='example-9'/>
+                <img className='map-of-new-artifacts__map-with-artifacts__img--example-10' src='./for-artifact-card/example-10.webp' alt='example-10'/>
                 <img className='map-of-new-artifacts__map-with-artifacts__img--example-11' src='./for-artifact-card/example-11.webp' alt='example-11'/>
                 <p className='artifact-card__rate map-of-new-artifacts__map-with-artifacts__rate--rate-1'>5.3</p>
                 <p className='artifact-card__rate map-of-new-artifacts__map-with-artifacts__rate--rate-2'>3.6</p>
                 <p className='artifact-card__rate map-of-new-artifacts__map-with-artifacts__rate--rate-3'>1.2</p>
+                <p className='artifact-card__rate map-of-new-artifacts__map-with-artifacts__rate--rate-4'>9.3</p>
             </div>
             <div className='map-of-new-artifacts__description-block'>
                 <h5 className='map-of-new-artifacts__description-block__heading'>Kurische
