2e5b356c17f82689bc831e2472c6aafd96e23451
simmetr <vm.certo@gmail.com>
2025-12-27 12:50:24 +0700

    test: добавлены тесты для пунктов меню навбара

--- test/CheckData.test.ts
+++ test/CheckData.test.ts
@@ -1,16 +1,37 @@
-import {describe, test} from "vitest";
+import {describe, expect, test} from "vitest";
 
 describe('Check nav bar menu items', () => {
+    const setOfMenuItemsInTheNavbar = [
+        [],
+        ['Лаборатория'],
+        ['Лаборатория', 'Новые артефакты'],
+        ['Лаборатория', 'Новые артефакты', 'Контакты'],
+        ['Лаборатория', 'Новые артефакты', 'Контакты', 'О нас'],
+        ['Лаборатория', 'Новые артефакты', 'Контакты', 'О нас', 'Бред сивой кобылы']
+    ];
     test('Количество пунктов в навбаре не более 4-х', () => {
-
+        expect.soft(() => new SetOfMenuItemsInTheNavbar(setOfMenuItemsInTheNavbar[0])).toThrowError(new ErrorCreatingMenuItems('empty list'));
+        expect.soft(() => new SetOfMenuItemsInTheNavbar(setOfMenuItemsInTheNavbar[1])).not.toThrowError();
+        expect.soft(() => new SetOfMenuItemsInTheNavbar(setOfMenuItemsInTheNavbar[2])).not.toThrowError();
+        expect.soft(() => new SetOfMenuItemsInTheNavbar(setOfMenuItemsInTheNavbar[3])).not.toThrowError();
+        expect.soft(() => new SetOfMenuItemsInTheNavbar(setOfMenuItemsInTheNavbar[4])).not.toThrowError();
+        expect.soft(() => new SetOfMenuItemsInTheNavbar(setOfMenuItemsInTheNavbar[5])).toThrowError(new ErrorCreatingMenuItems('too many menu items (there should be no more than four)'));
     });
 
-    test('Минимальная ширина блоков пунктов в навбаре не менее...', () => {
-
-    });
-
-    test('Тексты пукнтов навбара в порядке', () => {
-
+    test('Тексты пунктов навбара в порядке', () => {
+        const menuItemsInTheNavbar: SetOfMenuItemsInTheNavbar = new SetOfMenuItemsInTheNavbar(setOfMenuItemsInTheNavbar[4]);
+        test('Количество символов не превышает норму', () => {
+            menuItemsInTheNavbar.getItems.forEach((menuItem: string) =>
+                expect.soft(menuItem.length).toBeLessThan(10)
+            )
+        });
+        test('Каждый пункт не состоит из пробелов', () => {
+            const pattern = /^\s+$/g;
+            menuItemsInTheNavbar.getItems.forEach((menuItem: string) => {
+                expect.soft(menuItem.length).not.toBe('');
+                expect.soft(pattern.test(menuItem)).toBeTruthy();
+            });
+        });
     });
 });
 
