f01446b81fe517367a36c7001761cbfc1b62c71c
simmetr <vm.certo@gmail.com>
2025-12-27 19:07:47 +0700

    test: подправил тесты

--- test/CheckData.test.ts
+++ test/CheckData.test.ts
@@ -2,25 +2,37 @@ import {describe, expect, test} from "vitest";
 import {AppConfig} from "../src/model/AppConfig";
 import {IMenuItemsInTheNavBar} from "../src/model/IMenuItemsInTheNavBar";
 import {ErrorCreatingMenuItems} from "../src/model/ErrorCreatingMenuItems";
+import {ExampleOfMenuItems} from "../src/data/ExampleOfMenuItems";
 
 const getSamplesOfTheIMenuItemsInTheNavBarInterface = (): IMenuItemsInTheNavBar[] => {
-    return [];
+    return [
+        new ExampleOfMenuItems([]),
+        new ExampleOfMenuItems(['Контакты']),
+        new ExampleOfMenuItems(['Контакты', 'Лаборатория']),
+        new ExampleOfMenuItems(['Контакты', 'Лаборатория', 'Артефакты']),
+        new ExampleOfMenuItems(['Контакты', 'Лаборатория', 'Артефакты', 'Связь']),
+        new ExampleOfMenuItems(['Контакты', 'Лаборатория', 'Артефакты', 'Связь', 'Бред сивой кобылы']),
+    ];
 }
 
 describe('Проверка пунктов меню', () => {
-    describe(`Количество пунктов меню от ${AppConfig.takeTheMinimumNumberOfMenuItems()} до ${AppConfig.takeTheMaximumNumberOfMenuItems()}`, () => {
+    describe(`Количество пунктов меню от ${AppConfig.getTheMinimumNumberOfMenuItems()} до ${AppConfig.getTheMaximumNumberOfMenuItems()}`, () => {
         test('Особый случай: пустой массив образцов', () => {
             expect.soft(() => getSamplesOfTheIMenuItemsInTheNavBarInterface()[0].getItems()).toThrowError(new ErrorCreatingMenuItems('array is empty'));
         });
 
-        test.each(getSamplesOfTheIMenuItemsInTheNavBarInterface().slice(1))('Обработка массива %s', (array: IMenuItemsInTheNavBar) => {
-            expect.soft(array.getItems().length).toBeLessThanOrEqual(AppConfig.takeTheMinimumNumberOfMenuItems());
-            expect.soft(array.getItems().length).toBeGreaterThanOrEqual(AppConfig.takeTheMaximumNumberOfMenuItems());
+        test('Особый случай: всего один элемент в массиве ', () => {
+            expect.soft(() => getSamplesOfTheIMenuItemsInTheNavBarInterface()[1].getItems()).toThrowError(new ErrorCreatingMenuItems(`minimum number of menu items - ${AppConfig.getTheMinimumNumberOfMenuItems()}`));
+        });
+
+        test.each(getSamplesOfTheIMenuItemsInTheNavBarInterface().slice(2, 5))('Обработка массива %s', (array: IMenuItemsInTheNavBar) => {
+            expect.soft(array.getItems().length).toBeGreaterThanOrEqual(AppConfig.getTheMinimumNumberOfMenuItems());
+            expect.soft(array.getItems().length).toBeLessThanOrEqual(AppConfig.getTheMaximumNumberOfMenuItems());
         });
     });
 
     describe('Пункты меню в нормальном виде', () => {
-        test.each(getSamplesOfTheIMenuItemsInTheNavBarInterface().slice(1))('Обработка пункта "%s"', (array: IMenuItemsInTheNavBar) => {
+        test.each(getSamplesOfTheIMenuItemsInTheNavBarInterface().slice(2, 5))('Обработка пункта "%s"', (array: IMenuItemsInTheNavBar) => {
             const pattern = /^\s+$/g;
             array.getItems().map((item: string) => {
                 expect.soft(pattern.test(item)).toBeFalsy();
